268d2ac4d9426152f396610909b423b6bf05a0a6
St0wy <stawer1911@gmail.com>
2019-03-06 10:56:25 +0100

    début mode dynamique pour ecg

--- MFECGSignal/MFECGSignal/ECGSignal.cs
+++ MFECGSignal/MFECGSignal/ECGSignal.cs
@@ -34,54 +34,75 @@ namespace MFECGSignal
         public static void Main()
         {
             //one heartbeat 272.72ms
+            #region ECG
             int[] ecgValues = new int[]{238, 238, 238, 238, 238, 238, 238, 248, 258, 269, 280, 294, 308, 313, 318, 313, 308, 301, 294, 287, 280, 273, 266, 259,
-                                              252, 245, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 227, 217, 199, 182, 255, 329, 444, 560, 672, 784, 871,
-                                              959, 773, 588, 301, 014, 059, 105, 161, 217, 227, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238,
-                                              238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 248, 259, 266, 273, 280, 287, 294, 301, 308, 315, 322,
-                                              329, 336, 343, 346, 350, 353, 357, 360, 364, 360, 357, 353, 350, 346, 343, 336, 329, 322, 315, 304, 294, 280, 266, 252,
-                                              238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 248, 259, 262, 266, 259, 252, 245, 238, 238, 238, 238, 238, 238, 238};
+                                        252, 245, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 227, 217, 199, 182, 255, 329, 444, 560, 672, 784, 871,
+                                        959, 773, 588, 301, 014, 059, 105, 161, 217, 227, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238,
+                                        238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 248, 259, 266, 273, 280, 287, 294, 301, 308, 315, 322,
+                                        329, 336, 343, 346, 350, 353, 357, 360, 364, 360, 357, 353, 350, 346, 343, 336, 329, 322, 315, 304, 294, 280, 266, 252,
+                                        238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 248, 259, 262, 266, 259, 252, 245, 238, 238, 238, 238, 238, 238, 238}; 
+            #endregion
 
-            const long TIME_MS_TO_TICK = 10000;
+            #region Constants
+            const long TIME_ONE_MS_IN_TICK = 10000;
+            const int TIME_ONE_MIN_IN_MS = 60000;
             const int STATE_DYNAMIC = 0;
             const int STATE_STATIC = 1;
             const long LENGTH_ONE_HEARTBEAT_IN_TICK = 2727272;
+            const int AVG_ECG_VALUE = 238;
+            const int BPM_HEART_MAX = 220;
+            const int BPM_HEART_MIN = 40;
+            #endregion
 
+            #region Var
+            Button btnJoy = new Button(true, new InputPort(GHICard.Socket10.Pin3, false, Port.ResistorMode.Disabled), 100);
+            AnalogInput joyX = new AnalogInput(GHICard.Socket10.AnalogInput4);
+            joyX.Scale = BPM_HEART_MAX - BPM_HEART_MIN;
+            joyX.Offset = BPM_HEART_MIN;
             AnalogOutput analogOut = new AnalogOutput(GHICard.Socket9.AnalogOutput);
             int state = STATE_STATIC;
-            long timeToWait = 1000;  //in MS
+            long timeToWait = 10000000;  //in tick
             int index = 0;
             long ticksAtLastChange = 0;
             long ticksAtLastWait = 0;
-            bool isBeating = true;
+            bool isBeating = true; 
+            #endregion
 
             //Main Loop
             while (true)
             {
-                long ticks = Utility.GetMachineTime().Ticks;
+                //Inputs management
+                if (btnJoy.isPressed())
+                {
+                    if (state == STATE_DYNAMIC)
+                    {
+                        state = STATE_STATIC;
+                    }
+                    else if (state == STATE_STATIC)
+                    {
+                        state = STATE_DYNAMIC;
+                    }
+                }
 
                 switch (state)
                 {
                     case STATE_DYNAMIC:
-                        
+                        int bpm = joyX.ReadRaw();
+                        long timeTotalHeartbeat = bpm * LENGTH_ONE_HEARTBEAT_IN_TICK;
+                        timeToWait = (TIME_ONE_MIN_IN_MS * TIME_ONE_MS_IN_TICK) - timeTotalHeartbeat;
                         break;
                     case STATE_STATIC:
-                        if (isBeating)
-                        {
-                            analogOut.WriteRaw(ecgValues[index]);
-                        }
-                        else
-                        {
-
-                        }
                         
-                        break;
                     default:
                         break;
                 }
+
+                //Outputs management
+                long ticks = Utility.GetMachineTime().Ticks;
                 if (isBeating)
                 {
                     long elapsedTimeHeartbeat = ticks - ticksAtLastChange;
-                    if (elapsedTimeHeartbeat >= LENGTH_ONE_HEARTBEAT_IN_TICK / ecgValues.Length )
+                    if (elapsedTimeHeartbeat >= LENGTH_ONE_HEARTBEAT_IN_TICK / ecgValues.Length)
                     {
                         if (index == ecgValues.Length - 1)
                         {
@@ -100,12 +121,24 @@ namespace MFECGSignal
                 else
                 {
                     long elapsedTimeWait = ticks - ticksAtLastWait;
-                    if (elapsedTimeWait >= timeToWait * TIME_MS_TO_TICK)
+                    if (elapsedTimeWait >= timeToWait)
                     {
                         isBeating = true;
                         ticksAtLastChange = ticks;
                     }
                 }
+
+                //View
+                if (isBeating)
+                {
+                    analogOut.WriteRaw(ecgValues[index]);
+                }
+                else
+                {
+                    analogOut.WriteRaw(AVG_ECG_VALUE);
+                }
+
+                
             }//End while
         }
     }
