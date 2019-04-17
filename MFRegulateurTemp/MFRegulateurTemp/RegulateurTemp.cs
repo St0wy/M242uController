8dd25875fb38e12b438e08032fafd8b324ad3e05
St0wy <stawer1911@gmail.com>
2019-04-17 10:39:13 +0200

    début captation de la température

--- MFRegulateurTemp/MFRegulateurTemp/RegulateurTemp.cs
+++ MFRegulateurTemp/MFRegulateurTemp/RegulateurTemp.cs
@@ -30,9 +30,47 @@ namespace MFRegulateurTemp
 {
     public class RegulateurTemp
     {
+        const int VOLTAGE_0_DEGREE = 500; //in mV
+        const int TEMPERATURE_COEFFICIENT = 10; //in mV
+        const int HIGHEST_VOLTAGE = 330;
+        const int BUFFER_LENGHT = 3;
+
+        static Button btnSetting = new Button(new InputPort(GHICard.Socket9.Pin3, true, Port.ResistorMode.Disabled));
+        static Button btnRemove = new Button(new InputPort(GHICard.Socket12.Pin3, true, Port.ResistorMode.Disabled));
+        static Button btnAdd = new Button(new InputPort(GHICard.Socket14.Pin3, true, Port.ResistorMode.Disabled));
+
+        static AnalogInput heatSensor = new AnalogInput(GHICard.Socket10.AnalogInput3);
+        static double temperature = 0;
+
         public static void Main()
         {
-            
+            double oldtemp = 0;
+            double oldoldtemp = 0;
+
+            while (true)
+            {
+                int heatSensorValue = heatSensor.ReadRaw();
+                oldoldtemp = oldtemp;
+                oldtemp = temperature;
+                temperature = (heatSensorValue * HIGHEST_VOLTAGE) / System.Math.Pow(2, GHICard.SupportedAnalogInputPrecision) - (VOLTAGE_0_DEGREE / TEMPERATURE_COEFFICIENT);
+                temperature = moyDouble(new double[] { oldoldtemp, oldtemp, temperature });
+
+                Debug.Print(temperature.ToString());
+
+                Thread.Sleep(10);
+            }
+        }
+
+        public static double moyDouble(double[] tab)
+        {
+            double moy = 0;
+            foreach (double nbr in tab)
+            {
+                moy += nbr;
+            }
+            moy /= tab.Length;
+
+            return moy;
         }
     }
 }
