05285df9f440034fd9e6505af52d5a1c851a3cac
St0wy <stawer1911@gmail.com>
2019-04-17 11:36:24 +0200

    continuation temp

--- MFRegulateurTemp/MFRegulateurTemp/RegulateurTemp.cs
+++ MFRegulateurTemp/MFRegulateurTemp/RegulateurTemp.cs
@@ -33,14 +33,24 @@ namespace MFRegulateurTemp
         const int VOLTAGE_0_DEGREE = 500; //in mV
         const int TEMPERATURE_COEFFICIENT = 10; //in mV
         const int HIGHEST_VOLTAGE = 330;
-        const int BUFFER_LENGHT = 3;
+        const int TIMER_LENGHT = 20000;
+        const int LOOP_LENGHT = 20;
+        const int DEFAULT_LIMIT_TEMPERATURE = 20;
+        const int DEFAULT_HYSTERESIS = 10;
+
+        enum State { SettingsOff, SettingsTemperature, SettingsHysteresis, IncrementTemperature, DecrementTemperature, IncrementHysteresis, DecrementHysteresis };
 
         static Button btnSetting = new Button(new InputPort(GHICard.Socket9.Pin3, true, Port.ResistorMode.Disabled));
         static Button btnRemove = new Button(new InputPort(GHICard.Socket12.Pin3, true, Port.ResistorMode.Disabled));
         static Button btnAdd = new Button(new InputPort(GHICard.Socket14.Pin3, true, Port.ResistorMode.Disabled));
 
+        static State state = State.SettingsOff;
+
         static AnalogInput heatSensor = new AnalogInput(GHICard.Socket10.AnalogInput3);
         static double temperature = 0;
+        static int timer = 0;
+        static int limitTemperature = DEFAULT_LIMIT_TEMPERATURE;
+        static int hysteresis = DEFAULT_HYSTERESIS;
 
         public static void Main()
         {
@@ -55,9 +65,79 @@ namespace MFRegulateurTemp
                 temperature = (heatSensorValue * HIGHEST_VOLTAGE) / System.Math.Pow(2, GHICard.SupportedAnalogInputPrecision) - (VOLTAGE_0_DEGREE / TEMPERATURE_COEFFICIENT);
                 temperature = moyDouble(new double[] { oldoldtemp, oldtemp, temperature });
 
+                switch (state)
+                {
+                    case State.SettingsOff:
+                        if (btnSetting.isPressed())
+                        {
+                            timer = 0;
+                            state = State.IncrementTemperature;
+                        }
+                        break;
+                    case State.SettingsTemperature:
+                        timer += LOOP_LENGHT;
+
+                        if (btnAdd.isPressed())
+                        {
+                            state = State.IncrementTemperature;
+                        }
+                        if (btnRemove.isPressed())
+                        {
+                            state = State.DecrementTemperature;
+                        }
+                        if (timer >= TIMER_LENGHT)
+                        {
+                            state = State.SettingsOff;
+                        }
+                        if (btnSetting.isPressed())
+                        {
+                            timer = 0;
+                            state = State.SettingsHysteresis;
+                        }
+                        break;
+                    case State.SettingsHysteresis:
+                        timer += LOOP_LENGHT;
+
+                        if (btnAdd.isPressed())
+                        {
+                            state = State.IncrementHysteresis;
+                        }
+                        if (btnRemove.isPressed())
+                        {
+                            state = State.DecrementHysteresis;
+                        }
+                        if (btnSetting.isPressed() || timer >= TIMER_LENGHT)
+                        {
+                            state = State.SettingsOff;
+                        }
+                        break;
+                    case State.IncrementTemperature:
+                        limitTemperature++;
+
+                        state = State.SettingsTemperature;
+                        break;
+                    case State.DecrementTemperature:
+                        limitTemperature--;
+
+                        state = State.SettingsTemperature;
+                        break;
+                    case State.IncrementHysteresis:
+                        hysteresis++;
+
+                        state = State.SettingsHysteresis;
+                        break;
+                    case State.DecrementHysteresis:
+                        hysteresis--;
+
+                        state = State.SettingsHysteresis;
+                        break;
+                    default:
+                        break;
+                }
+
                 Debug.Print(temperature.ToString());
 
-                Thread.Sleep(10);
+                Thread.Sleep(LOOP_LENGHT);
             }
         }
 
